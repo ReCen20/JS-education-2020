dac99337ae47c38df80cef3be2e57f8c995b546b
ReCen20 <alpha55757@gmail.com>
2020-02-29 21:49:39 +0400

    third task

--- first task/script.js
+++ first task/script.js
@@ -1,21 +1,121 @@
 "use strict";
 
-let money = +prompt("Ваш бюджет на месяц", "0");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "1234-12-12");
+let money, time;
+
+function start() {
+    let moneyString = prompt("Ваш бюджет на месяц", "0");
+    time = prompt("Введите дату в формате YYYY-MM-DD", "1234-12-12");
+    money = +moneyString;
+
+    while(isNaN(money) || moneyString == "" || moneyString === null) {
+        moneyString = prompt("Ваш бюджет на месяц", "0");
+        money = +moneyString;
+    }
+}
+
+start();
+
 let appData = {
     money,
     time,
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
-let typeOfExpenses = prompt("Введите обязательную статью расходов в этом месяце", "");
-let moneyForExpenses = +prompt("Во сколько обойдется?", "0");
+function chooseExpenses() {
+    for(let i = 0; i < 2; i++) {
+        let typeOfExpenses = prompt("Введите обязательную статью расходов в этом месяце", "");
+        let moneyForExpenses = prompt("Во сколько обойдется?", "0");
+    
+        if((typeof(typeOfExpenses) != null) && (typeof(moneyForExpenses) != null) &&
+        (typeOfExpenses != "") && (moneyForExpenses != "")) {
+            console.log("done");
+            appData.expenses[typeOfExpenses] = moneyForExpenses;
+        }
+        else {
+            i--;
+        }
+    }    
+}
 
-appData.expenses[typeOfExpenses] = moneyForExpenses;
+function chooseOptExpenses () {
+    for(let i = 1; i < 4; i++) {
+        let typeOfExpenses = prompt("Введите опциональную статью расходов в этом месяце", "");
+        console.log(typeof(typeOfExpenses) != null);
+        console.log(typeof(typeOfExpenses));
+        if((typeof(typeOfExpenses) === "string") && (typeOfExpenses != "")) {
+            console.log("done");
+            appData.optionalExpenses[i] = typeOfExpenses;
+        }
+        else {
+            i--;
+        }
+    }
+}
 
-alert(`Ваш бюджет на один день равен ${(appData.money - appData.expenses[typeOfExpenses]) / 30}`);
+chooseOptExpenses();
 
+/*let i = 0;
+
+do {
+    let typeOfExpenses = prompt("Введите обязательную статью расходов в этом месяце", "");
+    let moneyForExpenses = prompt("Во сколько обойдется?", "0");
+    if((typeof(typeOfExpenses)=== "string") && (typeof(typeOfExpenses) != null) &&
+    (typeof(moneyForExpenses) != null) && (typeOfExpenses != "") && (moneyForExpenses != "")) {
+        console.log("done");
+        appData.expenses[typeOfExpenses] = moneyForExpenses;
+    }
+    else {
+        i--;
+    }
+    i++;
+} while (i < 2);
+
+while (i < 2) {
+    let typeOfExpenses = prompt("Введите обязательную статью расходов в этом месяце", "");
+    let moneyForExpenses = prompt("Во сколько обойдется?", "0");
+    if((typeof(typeOfExpenses)=== "string") && (typeof(typeOfExpenses) != null) &&
+    (typeof(moneyForExpenses) != null) && (typeOfExpenses != "") && (moneyForExpenses != "")) {
+        console.log("done");
+        appData.expenses[typeOfExpenses] = moneyForExpenses;
+    }
+    else {
+        i--;
+    }
+    i++;
+}*/
+
+function detectDayBudget () {
+    appData.moneyPerDay = Math.floor(appData.money / 30);
+
+    alert(`Ваш бюджет на один день равен ${appData.moneyPerDay}`);
+}
+
+function detectLevel () {
+    if(appData.moneyPerDay < 100) {
+        console.log("Минимальный уровень достатка");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("Высокий уровень достатка");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+
+
+
+function checkSavings () {
+    if(appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений","0");
+        let percent = +prompt("Под какой процент", "0");
+
+        appData.monthIncome = save/100/12*percent;
+        alert(`Доход в месяц с вашего депозита ${appData.monthIncome}`);
+    }
+}
+
+checkSavings();
 
