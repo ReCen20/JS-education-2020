8b09d80142b97467a0fe895425b567b388dfe984
ReCen20 <alpha55757@gmail.com>
2020-03-11 23:41:43 +0400

    Seventh task

---
+++ Fifth task/Part 2/js/index.js
@@ -0,0 +1,15 @@
+let start = document.querySelector("#start"),
+    budgetValue = document.querySelector(".budget-value"),
+    daybudgetValue = document.querySelector(".daybudget-value"),
+    levelValue = document.querySelector(".level-value"),
+    expensesValue = document.querySelector(".expenses-value"),
+    optionalExpensesValue = document.querySelector(".optionalexpenses-value"),
+    incomeValue = document.querySelector(".income-value"),
+    monthSavingsValue = document.querySelector(".monthsavings-value"),
+    yearSavingsValue = document.querySelector(".yearsavings-value");
+    
+let inputExpensesValue = document.querySelectorAll(".expenses-item"),
+    inputOptExpensesValue = document.querySelectorAll(".optionalexpenses-item"),
+    buttonApproveExp = document.querySelector("button"),
+    buttonApproveOptExp = document.querySelectorAll("button")[1],
+    buttonCalculate = document.querySelectorAll("button")[2];
